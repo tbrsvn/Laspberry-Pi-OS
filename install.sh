f0fd234b51d29d765e52fbc9e7c96c5f3fd3063a
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-05-31 12:34:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-echo "Starting Installation"
+echo "Installing..."
 sudo apt update && sudo apt upgrade
 sudo apt install lxqt-core git python3 python pip lxqt oxygen-icon-theme xorg xinit xserver-xorg-video-fbdev lightdm network-manager-gnome --no-install-recommends
 sudo systemctl enable lightdm
 sudo systemctl enable NetworkManager
 sudo systemctl set-default graphical.target
+echo "Done! Rebooting in 5 seconds!"
+sleep 5
 reboot
