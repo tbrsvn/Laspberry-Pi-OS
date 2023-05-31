4d1ca1623ed8b9efdcce5726e1c68b0add27ba86
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-05-31 12:34:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Installing..."
 sudo apt update && sudo apt upgrade
-sudo apt install lxqt-core git python3 python pip lxqt oxygen-icon-theme xorg xinit xserver-xorg-video-fbdev lightdm network-manager-gnome --no-install-recommends
+sudo apt install lxqt-core python3 python pip lxqt oxygen-icon-theme xorg xinit xserver-xorg-video-fbdev lightdm network-manager-gnome --no-install-recommends
 sudo systemctl enable lightdm
 sudo systemctl enable NetworkManager
 sudo systemctl set-default graphical.target
